b13919b6a13d5305431b69b65294f4aa3689abbf
TheCyberQuake <cyberquake101@gmail.com>
2019-09-05 16:40:03 -0700

    Initial Commit
    First commit for ServerHack

---
+++ Readme.txt
@@ -0,0 +1,19 @@
+ServerHack is a tool to allow Switch and Xbox Minecraft to connect to
+a custom dedicated bedrock server. It works by using a DNS server
+to hijack one of the Featured Servers and make it our own. 
+
+Usage:
+  1.  Launch ServerHack.bat
+  2.  Click yes when it asks if you want to allow it to make changes
+  3.  When prompted, type in the server URL or IP you wish to connect to
+  4.  The window should display what to set as the DNS server on your switch
+  5.  On the switch, go to System Settings, then Internet
+  6.  Go to Internet Settings, Then choose your usual network connection
+  7.  Select Change Settings, DNS Settings, and set to Manual
+  8.  Set Primary DNS and Secondary DNS as instructed by step 4 above
+  9.  Click save, then back out of the menu
+  10. Open Minecraft
+  11. Once signed in to your Microsoft account, click Play
+  12. Go to Servers, then load The Hive
+  13. If everything is working, you should now be loaded into your server
+ 
---
+++ ServerHack.bat
@@ -0,0 +1,166 @@
+@echo off
+color 3f
+title Minecraft Server Redirection Hack v1.2
+
+:: We escalate to Admin due to being a requirement for AdGuard Home
+
+:::::::::::::::::::::::::::::
+::Privilege Check/Escalation
+:::::::::::::::::::::::::::::
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+:::::::::::::::::::::::::::::
+::Script Start
+:::::::::::::::::::::::::::::
+
+if not exist "%CD%\DNS\AdGuardHome.exe" (
+  color 8f
+  echo Downloading AdGuard Home...
+  powershell -nologo -noprofile -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_Windows_amd64.zip', 'DNS.zip')"
+  powershell -nologo -noprofile -Command "Invoke-WebRequest https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_Windows_amd64.zip -OutFile DNS.zip"
+  if not exist DNS mkdir DNS && echo Creating DNS folder
+  echo Decompressing download...
+  powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%CD%\DNS'); $zip = $shell.NameSpace('%CD%\DNS.zip'); $target.CopyHere($zip.Items(), 16); }
+  del "%CD%\DNS.zip"
+  goto launch
+)
+
+
+:: Variable to keep track of when we need to skip lines
+set skipnow = 0
+
+:: Set working directory to the location of this script, fix for running as admin
+cd /d "%~dp0"
+
+:: Clear previous query log
+copy /y NUL "%CD%\DNS\data\querylog.json" >NUL
+
+:: Create Server.txt if it doesn't exist, prevents error at next step
+if not exist "%CD%\Server.txt" copy /y NUL Server.txt >NUL
+
+:: Pull saved server, if exists bypass asking for server info
+set /p saved=<"%CD%\Server.txt"
+if "%saved%" == "" (
+  set /p server="What server do you wish to connect to?: "
+) else (
+  set server=%saved%
+)
+
+cls
+
+:: Get the IP of the Server
+for /f "tokens=1,2,3 delims= " %%A in ('ping -4 -n 1 %server% ^| find "Pinging"') do ( 
+  if "%%C" == "with" (
+	set ServerIP=%%B
+  ) else (
+    set str=%%C
+	set ServerIP=!str:~1,-1!
+  )
+)
+
+:: Get IP of current machine
+for /f "delims=[] tokens=2" %%A in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%A
+
+:: Ensure the Server is valid and reachable (responds to pings)
+if not "%ServerIP%" == "" (
+  cls
+  ping -4 -n 1 !ServerIP! | find "TTL=" >nul
+  if errorlevel 1 (
+    color 4f
+    echo ERROR: Could not resolve IP for %server%. Is it a valid address?
+	if not "%saved%" == "" echo Delete Server.txt if you wish to stop auto-connecting to this server
+    pause
+    exit /B 1
+  )
+) else (
+  color 4f
+  echo ERROR: Could not resolve IP for %server%. Is it a valid address?
+  if not "%saved%" == "" echo Delete Server.txt if you wish to stop auto-connecting to this server
+  pause
+  exit /B 1
+)
+
+
+:: Give server info, as well as dialog on how to stop auto-connect if currently active
+echo Set to connect to %server%
+if not "%saved%" == "" echo Delete Server.txt if you wish to stop auto-connecting to this server
+
+:: Rename AdGuardHome.yaml to .bak, prepares for writing new ruleset
+move /Y "%cd%\DNS\AdGuardHome.yaml" "%CD%\DNS\AdGuardHome.yaml.bak" >NUL
+
+:: Go through the old yaml.bak line by line, modify what's needed to update the ruleset, output to new file
+for /F "usebackq delims=" %%A in ("%cd%\DNS\AdGuardHome.yaml.bak") do (
+  if "%%A" == "user_rules: []" (
+	echo user_rules:>> "%cd%\DNS\AdGuardHome.yaml"
+    echo - %ServerIP% hivebedrock.network>> "%cd%\DNS\AdGuardHome.yaml"
+  ) else (
+    if "%%A" == "user_rules:" (
+	  echo user_rules:>> "%cd%\DNS\AdGuardHome.yaml"
+	  echo - %ServerIP% hivebedrock.network>> "%cd%\DNS\AdGuardHome.yaml"
+	  set /a skipnow=1
+	) else (
+	  if "!skipnow!" == "0" (
+	    echo.%%A>> "%cd%\DNS\AdGuardHome.yaml"
+	  ) else (
+	    set /a skipnow=0
+	  )
+	)
+  )
+)
+
+:: Put the ruleset in filters, probably not necessary
+echo %ServerIP% hivebedrock.network>"%CD%\DNS\data\filters\0.txt"
+echo.
+echo.
+
+:: Tell user what settings to use for DNS on device
+echo Please add "%NetworkIP%" as the primary DNS on your device,
+echo and add "1.1.1.1" as the secondary DNS
+echo.
+echo.
+
+:launch
+:: Open firstrun.rtf for Instructions for initial AdGuard setup
+if not exist "%CD%\DNS\AdGuardHome.yaml" write.exe firstrun.rtf
+
+:: Launch AdGuardHome to do the DNS server
+"%CD%\DNS\AdGuardHome.exe"
+pause
